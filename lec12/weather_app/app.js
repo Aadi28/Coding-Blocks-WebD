78a33e389d95724edb799fe4f6760f4f27c30192
Aadi28 <aaditya280898@gmail.com>
2020-08-17 14:29:37 +0530

    weather app

--- lec12/weather_app/app.js
+++ lec12/weather_app/app.js
@@ -1,17 +1,27 @@
-$(document).ready(() => {
-    $("#button").click(() => {
-        var input_value = $("#inp").val();
-        console.log(input_value)
-    })
+const button = document.querySelector("#button")
+const name_city = document.querySelector(".name")
+const temperature = document.querySelector(".temp")
+const description = document.querySelector(".desc")
+const high_low = document.querySelector(".hi-low")
 
-    // $(".output").append(data.toString())
+const appid = 'd2238a3b3699ea69ff0d91d667725f5b'
+
+button.addEventListener("click", () => {
+    const inp = document.querySelector(".city").value
+    document.querySelector("#inp").innerHTML = ""
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inp}&appid=${appid}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            var nameVal = data["name"] + ", " + data["sys"]["country"];
+            var temp = Math.round(data["main"]["temp"]);
+            var desc = data["weather"][0]["description"];
+            var hi_low = data["main"]["temp_min"] + "&degC/ " + data["main"]["temp_max"] + "&degC"
+            var icon = data["weather"][0]["icon"];
+            name_city.innerHTML = nameVal;
+            temperature.innerHTML = temp + "&degC" + `<img src="http://openweathermap.org/img/wn/10d@${icon}.png"></img>`;
+            description.innerHTML = desc;
+            high_low.innerHTML = hi_low;
+        });
 })
 
-// document.getElementById("button").addEventListener("click", getVar)
-
-// function getVar() {
-//     var inp = document.getElementById("inp").innerText
-//     console.log("button pressed")
-// }
-
-export { input_value };
